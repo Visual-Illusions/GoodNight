e0df5ee7a8a81b1be8ae7a8d53f2cb59bfa1b68b
darkdiplomat <darkdiplomat@visualillusionsent.net>
2014-12-20 10:23:14 -0600

    build number format change, reload permission correction, jar signing properties

--- src/main/java/net/visualillusionsent/goodnight/GoodNight.java
+++ src/main/java/net/visualillusionsent/goodnight/GoodNight.java
@@ -4,15 +4,15 @@
  * Copyright © 2012-2014 Visual Illusions Entertainment
  *
  * GoodNight is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU General Public License v3 as published by
  * the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
+ * See the GNU General Public License v3 for more details.
  *
- * You should have received a copy of the GNU General Public License along with this program.
+ * You should have received a copy of the GNU General Public License v3 along with this program.
  * If not, see http://www.gnu.org/licenses/gpl.html.
  */
 package net.visualillusionsent.goodnight;
